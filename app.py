4d9fc1162e7605bae8bc2948de2cc1ea9e7881e4
matthew-elia <matt.elia@gramercytech.com>
2016-06-11 07:28:57 -0400

    gitignore and app file

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
@@ -6,5 +9,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__ == '__main__':
     app.run()
+
