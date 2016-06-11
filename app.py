6eb3385c3c5e0e0ebbf89c3a741a9e8960aaaafb
matthew-elia <matt.elia@gramercytech.com>
2016-06-11 17:57:03 -0400

    change app.py routing // sanitized&organized scraped data

--- app.py
+++ app.py
@@ -13,32 +13,43 @@ db = SQLAlchemy(app)
 from models import QB_YTD_STATS
 
 
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
 		errors = []
 		results = {}
-		data_label = []
+		labels = []
+		stats = []
+
 		if request.method == "POST":
 			  try:
 			  	url = request.form['url']
 			  	r = requests.get(url)
-			  	soup = BeautifulSoup(r.text, 'html.parser')
+			  	soup = BeautifulSoup(r.content, 'html.parser')
 			  except:
 			  	errors.append(
 			  		"Unable to get URL. Please make sure it's valid and try again."
 			  		)
 			  	return render_template('index.html', errors=errors)
 			  if r:
+						page_title = soup.find("tr", { "class" : "title" })
+						for title in page_title.findAll("td"):
+							print(title.get_text())
+
+						categories = soup.find("tr", { "id" : "special"})
+						for category in categories.findAll("td"):
+							print(category.get_text())
+
 						thead = soup.find("tr", { "class" : "label" })
 						for label in thead.findAll("a"):
-							for text in label:
-								print(text)
+							print(label.get_text())
+							labels.append(label.get_text())
+
 						table = soup.find("table", { "class" : "data" })
-						for row in table.findAll("tr"):
-							for cell in row.findAll("td"):
-								for data in cell:
-									print(data)
-									
-		return render_template('index.html', errors=errors,results=results)
+						for row in table.findAll("tr", { "class" : "row1" or "row2" }):
+							for data in row.findAll("td", { "class" : not "mybg4" }): 
+									print(data.get_text())
+									stats.append(data.get_text())
+
+		return render_template('index.html', errors=errors, results=results)
 if __name__ == '__main__':
 	app.run()
